62ba5c8c04784e5a693fe5ca21a8f7d2c29faf58
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2026-05-15 15:45:32 +0700

    feat: include new services

--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.routers.brand import router as brand_router
 from src.api.routers.model import router as model_router
-
 from src.api.routers.car import router as car_router
+from src.api.routers.auth import router as auth_router
+from src.api.routers.user import router as user_router
 
 from src.models.base import Base
 from src.db.session import engine
@@ -21,6 +22,8 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(router=auth_router)
+app.include_router(router=user_router)
 app.include_router(router=brand_router)
 app.include_router(router=model_router)
 app.include_router(router=car_router)
