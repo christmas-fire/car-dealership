c2ee2b10e1dd0ce379211cc886d5a90b7369ab22
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2026-05-15 15:46:32 +0700

    feat: add password hash and generate jwt

---
+++ src/core/security.py
@@ -0,0 +1,29 @@
+import bcrypt
+import jwt
+from datetime import datetime, timedelta, timezone
+
+from src.core.config import get_settings
+
+settings = get_settings()
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    password_bytes = plain_password.encode('utf-8')
+    hashed_password_bytes = hashed_password.encode('utf-8')
+
+    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
+
+
+def get_password_hash(password: str) -> str:
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password.decode('utf-8')
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
